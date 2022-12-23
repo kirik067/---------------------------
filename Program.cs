f9e2277c753b52c36a55060b5ef9670c76bdd09f
kirill <tvoag.2015@mail.ru>
2022-12-23 19:32:39 +0500

    изминение в коде решения

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿string[] strings = { "Hello", "2" , "world" , ":-)" }; 
-var result = strings.Where(s => s.Length <= 3).ToArray(); 
-Console.WriteLine(string.Join(", ", result)); 
+var result = strings.Where(s => s.Length <= 3).ToArray();
+Console.Write("[");
+Console.Write(string.Join(", ", result));
+Console.Write("]");
 Console.ReadLine();
 
