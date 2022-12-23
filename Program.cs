25426bd9173f928e76fdc6227ccf68da52338402
kirill <tvoag.2015@mail.ru>
2022-12-23 18:41:21 +0500

    zadaha 2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿string[] strings = { "Hello", "2" , "world" , ":-)" }; 
+var result = strings.Where(s => s.Length <= 3).ToArray(); 
+Console.WriteLine(string.Join(", ", result)); 
+Console.ReadLine();
+
